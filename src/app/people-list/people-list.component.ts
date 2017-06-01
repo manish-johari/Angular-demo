f730bd00c264000d0b854ab030ee25d354d51906
Manish <joharimani@gmail.com>
2017-06-01 10:44:58 +0530

    `ng generate component --inline-template people-list` Create our very first component: the PeopleListComponent.

---
+++ src/app/people-list/people-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-people-list',
+  template: `
+    <p>
+      people-list Works!
+    </p>
+  `,
+  styleUrls: ['./people-list.component.scss']
+})
+export class PeopleListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
