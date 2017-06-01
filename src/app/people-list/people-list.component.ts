61705a911a4e896fc2f6ba0befcb8354a4bc6ad7
Manish <joharimani@gmail.com>
2017-06-01 11:36:03 +0530

    Expose a people property with an array of Person with the mighty Luke, Darth Vader and Solo.

--- src/app/people-list/people-list.component.ts
+++ src/app/people-list/people-list.component.ts
@@ -11,6 +11,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PeopleListComponent implements OnInit {
 
+  people: Person[] = [
+    {name: 'Luke Skywalker', height: 177, weight: 70},
+    {name: 'Darth Vader', height: 200, weight: 100},
+    {name: 'Han Solo', height: 185, weight: 85},
+  ];
+
   constructor() { }
 
   ngOnInit() {
